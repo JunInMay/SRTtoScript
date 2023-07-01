d5555f48aa34d0354f66ff6e0c728f51c5069eb2
Chana <twszak01@gmail.com>
2023-07-02 01:53:12 +0900

    버튼 생성과 버튼 이벤트 생성 과정 분리

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+import java.util.prefs.Preferences;
 
 public class GUI extends JFrame {
     private Logic logic;
@@ -11,15 +12,17 @@ public class GUI extends JFrame {
     private static String fileName = "test";
     private static String extension = ".txt";
 
+    private String lastUsedFolder = ".";
+
     public GUI(Logic l) {
         setSize(700, 700);
         setTitle("Frame Test");
         setLayout(new FlowLayout());
         logic = l;
 
-        getContentPane().add(buttonFileChoose());
-        getContentPane().add(buttonExecute());
-        getContentPane().add(buttonFileGenerate());
+        getContentPane().add(button("Open", actionFileChoose()));
+        getContentPane().add(button("Execute", actionExecute()));
+        getContentPane().add(button("Generate", actionFileGenerate()));
     }
 
     public void engage () {
@@ -27,46 +30,47 @@ public class GUI extends JFrame {
         setVisible(true);
     }
 
-    public JButton buttonFileChoose () {
-        JButton fileChooseButton = new JButton("Open");
-        fileChooseButton.setPreferredSize(new Dimension(80, 30));
-        fileChooseButton.addActionListener(new ActionListener() {
+    private JButton button (String text, ActionListener actionListener) {
+        JButton button = new JButton(text);
+        button.setPreferredSize(new Dimension(80, 30));
+        button.addActionListener(actionListener);
+        button.setMargin(new Insets(1, 1, 1, 1));
+
+        return button;
+    }
+
+    private ActionListener actionFileChoose() {
+        return new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JFileChooser fileChooser = new JFileChooser();
+                Preferences prefs = Preferences.userRoot().node(getClass().getName());
+                JFileChooser fileChooser = new JFileChooser(prefs.get(lastUsedFolder,
+                        new File(".").getAbsolutePath()));
                 int returnValue = fileChooser.showOpenDialog(null);
                 if (returnValue == JFileChooser.APPROVE_OPTION) {
                     selectedFile = fileChooser.getSelectedFile();
+                    prefs.put(lastUsedFolder, fileChooser.getSelectedFile().getParent());
                 }
             }
-        });
-        return fileChooseButton;
+        };
     }
 
-    public JButton buttonExecute () {
-        JButton executeButton = new JButton("Execute");
-        executeButton.setPreferredSize(new Dimension(80, 30));
-        executeButton.addActionListener(new ActionListener() {
+    private ActionListener actionExecute() {
+        return new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println(logic.discernSentence(selectedFile));
             }
-        });
-
-        return executeButton;
+        };
     }
 
-    public JButton buttonFileGenerate () {
-        JButton generateButton = new JButton("Generate");
-        generateButton.setPreferredSize(new Dimension(80, 30));
-        generateButton.addActionListener(new ActionListener() {
+    private ActionListener actionFileGenerate() {
+        return new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 logic.makeFile(filePath, fileName, extension);
             }
-        });
-        generateButton.setMargin(new Insets(1,1,1,1));
-        return generateButton;
+        };
     }
 
 }
